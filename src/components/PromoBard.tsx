c92c45bcf675e06c0ae9b330900b2c4a0a8ddcbe
cryptocrayzz <161419857+cryptocrayzz@users.noreply.github.com>
2024-03-23 10:39:20 +0700

    Update PromoBard.tsx
    resized promobar

--- src/components/PromoBard.tsx
+++ src/components/PromoBard.tsx
@@ -5,37 +5,37 @@ const PromoBar: React.FC = () => {
     {
       name: "Get Your Spot !",
       imgSrc:
-        "https://i.ibb.co/KrRCZ2k/logotest6.png",
-      link: "https://t.me/Wickeths",
-      linkTitle: "SolX",
-    },
-   {
-      name: "Get Your Spot !",
-      imgSrc:
         "https://learn.g2.com/hs-fs/hubfs/plan%20gif%20marketing%20strategy.gif?width=500&name=plan%20gif%20marketing%20strategy.gif",
-      link: "https://t.me/Wickeths",
+      link: "https://twitter.com/Abblecoin",
       linkTitle: "SolX",
     },
     {
       name: "Get Your Spot !",
       imgSrc:
-        "https://i.ibb.co/KrRCZ2k/logotest6.png",
-      link: "https://t.me/Wickeths",
+        "https://pbs.twimg.com/profile_images/1681777814926766080/2NgQaPbr_400x400.jpg",
+      link: "https://twitter.com/KeroNFTs",
+      linkTitle: "SolX",
+    },
+    {
+      name: "Get Your Spot !",
+      imgSrc:
+        "https://www.ship.ph/wp-content/uploads/2017/04/Landscape-GIF-4.gif",
+      link: "https://twitter.com/SMLE_SOL",
       linkTitle: "SolX",
     },
   ];
 
   return (
-    <aside className="flex flex-col items-center md:items-stretch space-y-2 md:space-y-4 md:ml-4">
+    <aside className="flex flex-col items-center md:items-stretch space-y-2 md:space-y-2 md:ml-6">
       {cards.map((card, index) => (
         <div
           key={index}
-          className="flex flex-col justify-center items-center w-48 bg-muted border border-gray-200 rounded-lg shadow-md p-4"
+          className="flex flex-col justify-center items-center w-80 bg-muted border border-gray-200 rounded-lg shadow-md p-2"
         >
           <img
             src={card.imgSrc}
             alt={`Ad Spot ${index + 1}`}
-            className="h-32 mb-2 rounded-md"
+            className="h-40 mb-2 rounded-md"
           />
           <h3 className="text-xl font-bold mb-2">{card.name}</h3>
           <a href={card.link} target="_blank" rel="noopener noreferrer">
