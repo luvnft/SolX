c422d62e0c9ee7e99c710159c742a62b9293cad4
cryptocrayzz <161419857+cryptocrayzz@users.noreply.github.com>
2024-03-23 11:11:13 +0700

    Update PromoBard.tsx

--- src/components/PromoBard.tsx
+++ src/components/PromoBard.tsx
@@ -4,23 +4,20 @@ const PromoBar: React.FC = () => {
   const cards = [
     {
       name: "Get Your Spot !",
-      imgSrc:
-        "https://learn.g2.com/hs-fs/hubfs/plan%20gif%20marketing%20strategy.gif?width=500&name=plan%20gif%20marketing%20strategy.gif",
+      imgSrc: "https://learn.g2.com/hs-fs/hubfs/plan%20gif%20marketing%20strategy.gif?width=500&name=plan%20gif%20marketing%20strategy.gif",
       link: "https://t.me/Wickeths",
       linkTitle: "SolX",
     },
     {
       name: "Get Your Spot !",
-      imgSrc:
-        "https://pbs.twimg.com/profile_images/1681777814926766080/2NgQaPbr_400x400.jpg",
-      link: "https://t.me/Wickeths",
+      imgSrc: "https://pbs.twimg.com/profile_images/1681777814926766080/2NgQaPbr_400x400.jpg",
+      link: "https://twitter.com/KeroNFTs",
       linkTitle: "SolX",
     },
     {
       name: "Get Your Spot !",
-      imgSrc:
-        "https://www.ship.ph/wp-content/uploads/2017/04/Landscape-GIF-4.gif",
-      link: "https://t.me/Wickeths",
+      imgSrc: "https://www.ship.ph/wp-content/uploads/2017/04/Landscape-GIF-4.gif",
+      link: "https://twitter.com/SMLE_SOL",
       linkTitle: "SolX",
     },
   ];
@@ -45,6 +42,14 @@ const PromoBar: React.FC = () => {
           </a>
         </div>
       ))}
+      {/* Centered Promo Inquiry Button */}
+      <div className="mt-4 flex justify-center w-full">
+        <a href="https://t.me/Wickeths" target="_blank" rel="noopener noreferrer">
+          <button className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-full">
+            Contact Us for Promo Inquiries
+          </button>
+        </a>
+      </div>
     </aside>
   );
 };
